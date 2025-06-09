4b831bf14a8b01af2b78641830aa07e89bf0f911
suhaib2024 <suhaibarahim@gmail.com>
2025-06-09 15:17:57 +0400

    4th

---
+++ LEVEL1/ft_strlen/ft_strlen.c
@@ -0,0 +1,31 @@
+/*
+
+Assignment name  : ft_strlen
+Expected files   : ft_strlen.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that returns the length of a string.
+
+Your function must be declared as follows:
+
+int	ft_strlen(char *str);
+
+*/
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+    int i = 0;
+
+    while(str[i])
+    {
+        i++;
+    }
+    return i;
+}
+// int main ()
+// {
+//     printf("Length:%d",ft_strlen("hello"));
+// }
