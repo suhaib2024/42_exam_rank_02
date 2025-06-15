3e00895d72ad772d62ec77468bd8c15134a79ee1
suhaib2024 <suhaibarahim@gmail.com>
2025-06-15 13:31:33 +0400

    .

---
+++ LEVEL2/ft_strcspn/ft_strcspn.c
@@ -0,0 +1,39 @@
+/*
+
+Assignment name	: ft_strcspn
+Expected files	: ft_strcspn.c
+Allowed functions: None
+---------------------------------------------------------------
+
+Reproduce exactly the behavior of the function strcspn
+(man strcspn).
+
+The function should be prototyped as follows:
+
+size_t	ft_strcspn(const char *s, const char *reject);
+
+*/
+
+#include <stdlib.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+    size_t i;
+    size_t j;
+
+    i = 0;
+    while(s[i])
+    {
+        j = 0;
+        while(reject[j])
+        {
+          if(s[i] == reject[j])
+          {
+            return i;
+          }
+          j++;
+        }
+        i++;
+    }
+    return i;
+}
---
+++ LEVEL2/print_bits/print_bits.c
@@ -0,0 +1,31 @@
+/*
+
+Assignment name  : print_bits
+Expected files   : print_bits.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a function that takes a byte, and prints it in binary WITHOUT A NEWLINE
+AT THE END.
+
+Your function must be declared as follows:
+
+void	print_bits(unsigned char octet);
+
+Example, if you pass 2 to print_bits, it will print "00000010"
+
+*/
+
+#include <unistd.h>
+
+void	print_bits(unsigned char octet)
+{
+    int i;
+    char bit;
+
+    i = 8;
+    while(i--)
+    {
+        bit = ((octet >> i) & i) + '0';
+    }
+}
